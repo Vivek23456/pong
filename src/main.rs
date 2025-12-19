e7ca2481182fce10031e8b2a9128af14c3cc2205
Vivek23456 <vivekatkari399@gmail.com>
2025-12-19 10:57:22 +0530

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,97 @@
+ extern crate graphics;
+ extern crate opengl_graphics;
+ extern crate piston;
+
+ use std::process;
+ use piston::window::WindowSettings;
+ use piston::event_loop::{Events, EventSettings};
+ use piston::input::{Button ,Key , PressEvent , ReleaseEvent, RenderArgs, RenderEvent, UpdateArgs ,UpdateEvent};
+ use glutin_window::GlutinWindow;
+ use opengl_graphics::{GlGraphics, OpenGL};
+ 
+ pub struct App {
+     gl: GlGraphics,
+     left score : u32,
+     left_pos:i32,
+     left_pos:i32,
+     left_vel:i32,
+     right_score : i32,
+     right_pos:i32,
+        right_vel:i32,
+        bail_x:i32,
+        ball_y:i32,
+        vel_x:i32,
+        vel_y:i32,
+
+ }
+
+ impl App{
+    fn render(&mut self, args: &RenderArgs){
+        use graphics::*;
+        const BACKGROUND: [f32; 4] = [0.0, 0.5, 0.5, 1.0];
+        const FOREGROUND: [f32; 4] = [0.0, 0.0, 1.0, 1.0];
+    
+        let left = rectangel ::square(0.0, 0/0 ,50/0);
+        let left_pos = self.left_pos as f64;
+        let right = rectangle::square(0.0, 0.0, 50.0);
+        let right_pos = self.right_pos as f64;  
+
+        let ball = rectangle::square(0.0, 0.0, 10.0);
+        let ball_x = self.ball_x as f64;
+        let ball_y = self.ball_y as f64;
+
+        self.gl.draw(args.viewport(), |c, gl| {
+            clear(BACKGROUND, gl);
+            rectangle(FOREGROUND, left, c.transform.trans(-40.0 , left_pos),gl);
+            rectangle(FOREGROUND, right, c.transform.trans(args.width as f64 -10.0, right_pos), gl);
+            rectangle(FOREGROUND, ball,c.transform.trans(ball_x, ball_y), gl);
+        });
+    
+    }
+    fn update(&mut self, args: &UpdateArgs) {
+        if (self.left_vel == 1 && self.left_pos < 291)
+        || (self.left_vel == -1 && self.left_pos > 1)
+         {
+            self.left_pos += self.left_vel ;
+        }
+        if (self.right_vel == 1 && self.right_pos < 291)
+        || (self.right_vel == -1 && self.right_pos > 1)
+         {
+            self.right_pos += self.right_vel ;  
+        }   
+        self.ball_x += self.vel_x;
+        if self.ball_x > 502 {
+            self.vel_x = -self.vel_x;
+            if self.ball_y < self.right_pos || self.ball_y > self.right_pos + 50 {
+                self.left_score += 1;
+                if self.left_score >= 5 {
+                    println!("Left Player Wins!");
+                    process::exit(0);
+                }
+
+                self.ball_x = 250;
+                self.ball_y = 171;
+
+            }
+        }
+        if self.ball_x < 1 {
+            self.vel_x = -self.vel_x;
+            if self.ball_y < self.left_pos || self.ball_y > self.left_pos + 50 {
+                self.right_score += 1;
+                if self.right_score >= 5 {
+                    println!("Right Player Wins!");
+                    process::exit(0);
+                }
+
+                self.ball_x = 250;
+                self.ball_y = 171;
+                
+        }
+            }
+            self.ball_y += self.vel_y;
+            if self.ball_y > 342 || self.ball_y < 1 {
+                self.vel_y = -self.vel_y;
+    
+            }
+        }
+ }
